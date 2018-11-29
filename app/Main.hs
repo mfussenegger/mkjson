c209ca48d477e3e3a31f421d9b2928605bf5fed7
Mathias Fussenegger <f.mathias@zignar.net>
2018-11-29 19:30:47 +0100

    Add uuid1 provider

--- app/Main.hs
+++ app/Main.hs
@@ -12,6 +12,7 @@ import qualified Data.Text                  as T
 import qualified Data.Text.Read             as T
 import qualified Data.UUID                  as UUID
 import qualified Data.UUID.V4               as UUID4
+import qualified Data.UUID.V1               as UUID1
 import           Lib
 import           System.Environment         (getArgs)
 
@@ -26,9 +27,18 @@ parseColumnDefinition x =
     parts = T.splitOn "=" text
 
 
+uuid1 :: IO UUID.UUID
+uuid1 = do
+  uuid <- UUID1.nextUUID
+  case uuid of
+    (Just u) -> pure u
+    Nothing  -> uuid1
+
+
 lookupProvider :: Text -> IO Value
-lookupProvider "uuid4"            = String . UUID.toText <$> UUID4.nextRandom
-lookupProvider _                  = pure $ String "foobar"
+lookupProvider "uuid4" = String . UUID.toText <$> UUID4.nextRandom
+lookupProvider "uuid1" = String . UUID.toText <$> uuid1
+lookupProvider _       = pure $ String "foobar"
 
 
 main :: IO ()
