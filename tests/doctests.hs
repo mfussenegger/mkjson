7655c02e68251943ed49ee38a5c1e0ef9fbb5a8e
Mathias Fussenegger <f.mathias@zignar.net>
2018-12-09 17:18:59 +0100

    Use glob in doctest to discover source files

--- tests/doctests.hs
+++ tests/doctests.hs
@@ -1,11 +1,8 @@
 
-import Test.DocTest
+import           System.FilePath.Glob (glob)
+import           Test.DocTest
 
 main :: IO ()
-main = doctest
-  [ "-isrc"
-  , "src/Expr.hs"
-  , "src/Aeson.hs"
-  , "src/Fake.hs"
-  , "app/Main.hs"
-  ]
+main = do
+  sources <- glob "src/**/*.hs"
+  doctest ("app/Main.hs" : sources)
