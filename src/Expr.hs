e95576550a585bc82a7a8e5ab7fec90ae4737e18
Mathias Fussenegger <f.mathias@zignar.net>
2019-04-07 15:51:04 +0200

    Add array literal support
      ↪  mkjson obj=$(mkjson arr="array(randomInt(0, 10), randomInt(0, 20))")
      {"obj":{"arr":[3,5]}}

--- src/Expr.hs
+++ src/Expr.hs
@@ -21,7 +21,8 @@ import           Text.Parsec.Text        (Parser)
 data Expr = IntLiteral !Integer
           | DoubleLiteral !Scientific
           | StringLiteral !Text
-          | JsonLiteral !Value
+          | ObjectLiteral !Value
+          | ArrayLiteral !Value
           | FunctionCall !Function
           deriving (Show, Eq)
 
@@ -44,25 +45,26 @@ expr = literal <|> functionCall
 
 
 literal :: Parser Expr
-literal = number <|> stringLiteral <|> jsonLiteral
+literal = number <|> stringLiteral <|> objectLiteral <|> arrayLiteral
 
 
 showExpr :: Expr -> Text
 showExpr (StringLiteral s) = "\"" <> s <> "\""
 showExpr (IntLiteral n)    = T.pack . show $ n
 showExpr (DoubleLiteral n) = T.pack . show $ n
-showExpr (JsonLiteral s)   = TL.toStrict . TL.decodeUtf8 $ encode s
+showExpr (ObjectLiteral s)   = TL.toStrict . TL.decodeUtf8 $ encode s
+showExpr (ArrayLiteral a)   = TL.toStrict . TL.decodeUtf8 $ encode a
 showExpr (FunctionCall _)  = error "Can only convert literals to text representation"
 
 
-jsonLiteral :: Parser Expr
-jsonLiteral = do
+objectLiteral :: Parser Expr
+objectLiteral = do
   assignments <- between (char '{') (char '}') (assignment `sepBy` comma)
   let
     objStr = "{" <> T.intercalate ", " assignments <> "}"
   case decode' (TL.encodeUtf8 . TL.fromStrict $ objStr) of
     Nothing -> error $ "Invalid JSON string: " <> T.unpack objStr
-    Just v  -> pure $ JsonLiteral v
+    Just v  -> pure $ ObjectLiteral v
   where
     assignment = do
       key <- stringLiteral 
@@ -70,6 +72,19 @@ jsonLiteral = do
       value <- literal
       pure $ showExpr key <> ": " <> showExpr value
     colon = char ':' >> spaces
+    comma = char ',' >> spaces
+
+
+arrayLiteral :: Parser Expr
+arrayLiteral = do
+  elements <- between (char '[') (char ']') (element `sepBy` comma)
+  let
+    arrStr = "[" <> T.intercalate ", " elements <> "]"
+  case decode' (TL.encodeUtf8 . TL.fromStrict $ arrStr) of
+    Nothing -> error $ "Invalid JSON string: " <> T.unpack arrStr
+    Just v  -> pure $ ArrayLiteral v
+  where
+    element = showExpr <$> literal
     comma = char ',' >> spaces
 
 
@@ -138,9 +153,12 @@ ident = do
 -- Right (StringLiteral "")
 --
 -- >>> parseExpr "{}"
--- Right (JsonLiteral (Object (fromList [])))
+-- Right (ObjectLiteral (Object (fromList [])))
 --
 -- >>> parseExpr "{\"x\": 10, \"y\": {}}"
--- Right (JsonLiteral (Object (fromList [("x",Number 10.0),("y",Object (fromList []))])))
+-- Right (ObjectLiteral (Object (fromList [("x",Number 10.0),("y",Object (fromList []))])))
+--
+-- >>> parseExpr "[10, 20, 30]"
+-- Right (ArrayLiteral (Array [Number 10.0,Number 20.0,Number 30.0]))
 parseExpr :: Text -> Either ParseError Expr
 parseExpr = parse expr "(unknown)"
