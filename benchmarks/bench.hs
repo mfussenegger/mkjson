2837b18ea63810e1be5ccb19d06b493101bd38b6
Mathias Fussenegger <f.mathias@zignar.net>
2020-05-01 16:40:35 +0200

    Add missing dependency for benchmarks

--- benchmarks/bench.hs
+++ benchmarks/bench.hs
@@ -12,8 +12,7 @@ exec expr = runFakeT (Just 1) (eval expr)
 
 main :: IO ()
 main = defaultMain
-  [ bgroup "ids" [ bench "ulid" $ nfIO (exec "ulid")
-                 , bench "uuid1" $ nfIO (exec "uuid1")
+  [ bgroup "ids" [ bench "uuid1" $ nfIO (exec "uuid1")
                  , bench "uuid4" $ nfIO (exec "uuid4") ]
   , bgroup "numbers" [ bench "randomInt-0-100"  $ nfIO (exec "randomInt(0, 100)" )
                      , bench "randomInt-0-1000" $ nfIO (exec "randomInt(0, 1000)" ) ]
