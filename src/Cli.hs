188782dad3348bc4a9d0adc172960c55e16cf9cf
Mathias Fussenegger <f.mathias@zignar.net>
2025-04-05 12:08:48 +0200

    Support E notation in --num argument

--- src/Cli.hs
+++ src/Cli.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Cli
   ( Args(..)
@@ -13,7 +14,9 @@ import           Data.Bifunctor      (bimap)
 import qualified Data.Text           as T
 import qualified Expr                as E
 import           Options.Applicative hiding (Const)
-import           Text.Read           (readMaybe)
+import qualified Text.Parsec as P
+import qualified Text.Parsec.String as P
+import Data.Functor (($>))
 
 
 type Field = (T.Text, E.Expr)
@@ -31,13 +34,32 @@ data Amount = Const Int
             deriving (Show)
 
 
-parseNum :: ReadM Amount
-parseNum = eitherReader parseNum'
+mapLeft :: (a -> c) -> Either a b -> Either c b
+mapLeft f = either (Left . f) Right
+
+
+-- >>> P.parse numParser "<num>" "Inf"
+-- Right Infinite
+--
+-- >>> P.parse numParser "<num>" "200"
+-- Right (Const 200)
+--
+-- >>> P.parse numParser "<num>" "5e4"
+-- Right (Const 50000)
+numParser :: P.Parser Amount
+numParser = inf <|> integer
   where
-    parseNum' "Inf" = Right Infinite
-    parseNum' s     = case readMaybe s of
-      (Just n) -> Right (Const n)
-      Nothing  -> Left "Expected a number or `Inf` for infinite records"
+    inf = P.string "Inf" $> Infinite
+    integer = do
+      digits <- read @Int <$> P.many1 P.digit
+      power <- P.optionMaybe eNotation
+      pure . Const $ digits * maybe 1 (10 ^) power
+    eNotation :: P.Parser Int
+    eNotation = read @Int <$> (P.oneOf "eE" >> P.many1 P.digit)
+
+
+parseNum :: ReadM Amount
+parseNum = eitherReader $ \s -> mapLeft show $ P.parse numParser s s
 
 
 parseExpr :: String -> Either String Field
@@ -52,7 +74,7 @@ parseExpr s =
 
 args :: Parser Args
 args = Args
-  <$> optional 
+  <$> optional
     ( option auto
       ( long "seed"
       <> help "A seed for the random data generator"
@@ -62,12 +84,11 @@ args = Args
     <> value (Const 1)
     <> help "Number of records to generate. Use `Inf` for infinite records" )
   <*> many (argument (eitherReader parseExpr) (metavar "FIELDS..."))
-    
 
 
 parseArgs :: [String] -> IO Args
 parseArgs cliArgs = handleParseResult $ execParserPure defaultPrefs opts cliArgs
   where
     opts = info (args <**> helper)
-      ( fullDesc <> progDesc 
+      ( fullDesc <> progDesc
       "Generate random JSON records. Use <field>=<provider> pairs to specify the fields the records should have." )
